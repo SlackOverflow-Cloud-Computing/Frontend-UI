42af3aa9d3f45bbfcf70f7551f38bb21423aa954
Frank Wong <tw3047@columbia.edu>
2024-10-30 01:32:44 -0400

    add log out, show user profile

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import MainContent from './components/MainContent';
-<<<<<<< HEAD
 import Profile from './components/Profile'; // Import Profile component
 import Auth from './components/Auth';
-=======
-import Footer from './components/Footer';
-import Auth from './components/Auth'
-import Songs from './components/Songs';
->>>>>>> 8f640a9497cbed077a58f067cae157651668f2e8
 import './styles/App.css';  // Import global styles
 
 function Home() {
@@ -31,8 +25,6 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         {/* Spotify OAuth callback route */}
         <Route path="/auth" element={<Auth />} />
-
-        <Route path='/songs' element={<Songs />} />
         
         {/* Default 404 not found */}
         <Route path="*" element={<div>404 Page Not Found</div>} />
